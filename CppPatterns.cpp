ab3b2e456e3e7ae2d000ff3e8600a9227de98cfb
LeeSanderson <lee.sanderson@codurance.com>
2025-10-01 10:05:42 +0100

    Initial boilerplate CMake project

---
+++ CppPatterns.cpp
@@ -0,0 +1,12 @@
+﻿// CppPatterns.cpp : Defines the entry point for the application.
+//
+
+#include "CppPatterns.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
